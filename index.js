8fbf87e9ac12eee953d010a4a863ba36bc092f58
Raul <raulflores33@gmail.com>
2022-03-13 19:21:05 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string){
+    return "HELLO"
+    }
+    
+    function whisper(string){
+     return "hello"
+     }
+    
+    function logShout(string){
+        console.log(string.toUpperCase())
+    }
+    
+    function logWhisper(string){
+        console.log(string.toLowerCase())
+    }
+    
+
+    function sayHiToHeadphonedRoommate(string){
+        if (string.toUpperCase() === string){
+            return "YES INDEED!"
+
+        } if (string === "Let's have dinner together!") {
+            return "I would love to!"
+
+    
+
+        } 
+
+
+        return 'I can\'t hear you!'
+    }
+   
+    
